d70cfea10072bff4c07d9337778b91c9ff344c8e
makkunii <mace21218@gmail.com>
2023-03-17 00:40:51 +0400

    first commit

---
+++ assets/js/music/music-list.js
@@ -0,0 +1,50 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+    {
+      name: "Kaibigan mo",
+      artist: "makkunii",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Sabihin mo",
+      artist: "makkunii",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "I would love again",
+      artist: "makkunii",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "Andito lang ako",
+      artist: "makkunii",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "Tinamaan Sayo",
+      artist: "makkunii",
+      img: "music-5",
+      src: "music-5"
+    },
+    {
+      name: "I'm Sorry",
+      artist: "makkunii",
+      img: "music-6",
+      src: "music-6"
+    },
+  
+  ];
